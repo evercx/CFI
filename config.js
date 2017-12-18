a5e75c7a76f28bf90f5c5667e0ab8ef1d1a52379
evercx <306534137@qq.com>
2017-12-18 12:11:46 +0800

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-var IPAddress = {
-	mongodb:'mongodb://42.96.142.224:27017/PSOCFI/',
-	//mongodb:'mongodb://localhost:27017/CFI/',
-	requestURL:'127.0.0.1'
-};
-
-//exports.db = PSOCFI_DB;
-
-module.exports = IPAddress;
